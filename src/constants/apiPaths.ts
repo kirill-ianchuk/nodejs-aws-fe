62c1ee897276c45a8b2231843db1507145c249da
ianchuk <kirill.ianchuk@merck.com>
2020-12-06 16:44:16 +0300

    Added Cart API URL to apiPaths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://4xh4vm4dbf.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://fnycl1qikh.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'https://kirill-ianchuk-cart-api-dev.eu-west-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
